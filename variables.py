2a750c58c23e8eaefef11f55c58f1d9d9cd019d8
Cloud User <ec2-user@ip-172-31-83-254.ec2.internal>
2017-11-05 07:53:25 +0000

    varibuls add 1 comment

---
+++ variables.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+str = 'hello world'
+print str
+print str[0]
+print str[2:5]
+print str[2:]
+print str * 3
+print str + "with python"
+print "Hello, Python!"
+list = [ 'chenna', 'kesava', 'b', 'btech' ]
+print list
+print list[0]
+print list[1:3]
+name = 'chenna'
+age = '29'
+experience = 6
+
+print name
+print age
+print experience
+print name, age, experience
+print (name, age, experience)
+name,age,ex = "chenna",29,6
+print name, age, ex
