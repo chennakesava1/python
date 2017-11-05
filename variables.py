6ec8a1ab7c396e59d6e32826b7e4098d70152256
Cloud User <ec2-user@ip-172-31-83-254.ec2.internal>
2017-11-05 10:49:23 +0000

    varibules file 2 commit

--- variables.py
+++ variables.py
@@ -7,7 +7,7 @@ print str[2:]
 print str * 3
 print str + "with python"
 print "Hello, Python!"
-list = [ 'chenna', 'kesava', 'b', 'btech' ]
+list = [ 'chennaist', 'kesavalist', 'b', 'btechlist' ]
 print list
 print list[0]
 print list[1:3]
@@ -22,3 +22,24 @@ print name, age, experience
 print (name, age, experience)
 name,age,ex = "chenna",29,6
 print name, age, ex
+
+
+
+#tuple = ( 'abcd' , 786 ,  'tes' , 2.23 , 'john' ,  70.2  )
+tuple = ( 'abcd' , 123 , 'efg', 45.6, 'ijk', 789 )
+print tuple[0:5]
+
+
+
+
+##Dictionary
+dictionary = {}
+dictionary['one'] = "this is one"
+dictionary['two'] = " this is two"
+dictionary['theree'] = "this is there"
+print dictionary['one']
+print dictionary['theree']
+print dictionary.keys()
+
+print dictionary.values()
+
