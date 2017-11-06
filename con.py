21cb239da02e7e3f077b78d97d127895636f6d53
Cloud User <ec2-user@ip-172-31-83-254.ec2.internal>
2017-11-06 03:59:23 +0000

    added if-else-elif examples

--- con.py
+++ con.py
@@ -25,3 +25,39 @@ if inputs <= 4:
         print "given number below 4"
 else:
     print "given number not in 0 to 4"
+
+friends = raw_input("type a first later of ur friend name")
+if friends == 'm':
+    print ("Mounika")
+elif friends == 'p':
+    print ("Praveen")
+elif friends == 'r':
+    print ("rama rao")
+elif friends == 'k':
+    print ("Koti")
+else:
+    print (" your friends list need to update ")
+
+
+print "Thank you !"
+
+a = int(input("enter a: "))
+b = int(input("enter b: "))
+c = int(input("enter c: "))
+if a < b < c:
+    print ( "given numbers a < b < c")
+else: 
+    print ( " given numbers are not like a<b<c")
+
+
+ab = int(input("enter a number only: "))
+cd = int(input("enter a number morethen ab"))
+if ab < cd:
+    print("ab < cd")
+else:
+    print("ab > cd ")
+if ab < 10 and ab > 5:
+    print ( " ab value in betwin 5 to 10")
+else:
+    print (" ab value not inbetwin 5 to 10")
+
